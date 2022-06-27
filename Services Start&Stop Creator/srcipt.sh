e7fcdf8198d0935185a529de807d8ef3ff0991c2
Mateusz Matracki <matrackim323@gmail.com>
2022-06-28 00:47:21 +0200

    Update srcipt.sh

--- Services Start&Stop Creator/srcipt.sh
+++ Services Start&Stop Creator/srcipt.sh
@@ -1,1 +1,45 @@
+#!/bin/bash
 
+#ENTRY 
+echo "#########################################"
+echo "#       Services Start&Stop Creator     #"
+echo "#########################################"
+
+#QUESTION ABOUT SERVICES
+echo "Enter name of services:"
+echo "Remember about good sequence"
+echo "EXAMPLE: apache2 mysqld network"
+read -a services
+sleep 1
+echo "Name of services saved."
+echo " "
+
+#QUESTION ABOUT DIRECTORY TO CREATE SCRIPT FILES
+echo "Enter directory to create script files:"
+echo "EXAMPLE: /home/root"
+read path
+sleep 1
+echo ""
+echo "Path saved."
+echo ""
+
+fun_start ()
+{
+  echo "#!/bin/bash" > $path/services-start.sh
+  
+  for i in ${!services[@]}; 
+    do
+      echo "systemctl ${myArray[i]} start" > $path/services-start.sh
+    done
+}
+
+
+fun_stop ()
+{
+  echo "#!/bin/bash" > $path/services-stop.sh
+  
+  for i in ${!services[@]}; 
+    do
+      echo "systemctl ${myArray[i]} stop" > $path/services-stop.sh
+    done
+}
